c868adb83f54e6815e0049a64d9fe4be206a2644
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-06-17 12:49:48 +0700

    strcut models and types folder

--- book-exchange-backend-mvc/prisma/migrations/20240617043036_init/migration.sql
+++ book-exchange-backend-mvc/prisma/migrations/20240617043036_init/migration.sql
@@ -126,7 +126,6 @@ CREATE TABLE `UserTransactionStatus` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `UserTransactionStatus_transactionId_key`(`transactionId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
