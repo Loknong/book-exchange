970ecee9566fc2bb2bf3eae94647a585c81a5aca
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-09-09 15:59:18 +0700

    coomend unnecessary use

--- book-exchange-frontend/src/components/BookExplore2/BookExplorePage_2.tsx
+++ book-exchange-frontend/src/components/BookExplore2/BookExplorePage_2.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react"
-import { BookCard } from "../../models/Books";
-import axios from "axios";
+// import { useEffect, useState } from "react"
+// import { BookCard } from "../../models/Books";
+// import axios from "axios";
 
 
-interface BooksCard {
-    title: string;
-}
+// // interface BooksCard {
+// //     title: string;
+// // }
 
-const BookExplorePage_2 = () => {
-const [booksList, setBookList] = useState<BookCard[]>()
-    useEffect(() => {
+// // const BookExplorePage_2 = () => {
+// // const [booksList, setBookList] = useState<BookCard[]>()
+// //     useEffect(() => {
       
-    const fetchBooks = async () => {
-        const response = await axios.get("")
-    }
+// //     const fetchBooks = async () => {
+// //         const response = await axios.get("")
+// //     }
      
-    }, [])
+// //     }, [])
     
-  return (
-    <div>
+//   return (
+//     <div>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default BookExplorePage_2
+// export default BookExplorePage_2
